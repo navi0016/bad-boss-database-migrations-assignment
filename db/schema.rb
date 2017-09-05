459cb0c2a392da352bba06eede4cb3bfaff61f8f
Navi Mann <navdeep.k.mann@gmail.com>
2017-09-05 15:18:04 -0400

    renamed party_guests to widgets

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170905184125) do
+ActiveRecord::Schema.define(version: 20170905191446) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,7 +31,7 @@ ActiveRecord::Schema.define(version: 20170905184125) do
     t.integer "quanity"
   end
 
-  create_table "party_guests", force: :cascade do |t|
+  create_table "widgets", force: :cascade do |t|
     t.string  "first_name"
     t.string  "last_name"
     t.string  "dietary_restrictions"
